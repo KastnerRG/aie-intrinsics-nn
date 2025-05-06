d02049e175b2bf9715aec1917baf1a3010ec1f40
CameronArch <cameronarch598@gmail.com>
2025-05-06 14:51:10 -0700

    Updated Makefile to increase heap size to 2 KB. Fixed kernel header definitions

--- gemv_i32/aie/kernels.h
+++ gemv_i32/aie/kernels.h
@@ -3,7 +3,11 @@
 #ifndef FUNCTION_KERNELS_H
 #define FUNCTION_KERNELS_H
 
-void GemV(
+void GemV8(
+	input_window_int32 * __restrict in, 
+    output_window_int32 * __restrict out);
+
+void GemV4(
 	input_window_int32 * __restrict in, 
     output_window_int32 * __restrict out);
 
